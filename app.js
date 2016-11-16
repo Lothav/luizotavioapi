af6d59cabe34dd7cb2960489ece3060d67388da2
Luiz0tavio <luizotavioresende@hotmail.com>
2016-11-16 19:21:38 -0200

    show port

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ app.get('/', function (req, res) {
 });
 var port = process.env.PORT || 3000;
 app.listen( port , function () {
-    console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port ' + port);
 });
