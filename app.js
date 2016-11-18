aef7396335e716b2a09e0916ded3fb3289a8cdcd
Luiz0tavio <luizotrv@gmail.com>
2016-11-17 22:52:02 -0200

    change port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 var pg = require('pg'); /* Postgres */
 
 var WebSocketServer = require('ws').Server,
-    wss = new WebSocketServer({ port: process.env.PORT || 8080 });
+    wss = new WebSocketServer();
 
 var players = [];
 wss.keepAlive = true;
